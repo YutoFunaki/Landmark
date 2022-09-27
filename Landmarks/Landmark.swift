b239710d0ab4bb51ed3c37cb96e3d5b0085e86a7
YutoFunaki <104195830+YutoFunaki@users.noreply.github.com>
2022-09-27 22:43:19 +0900

    Landmark構造を定義

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -6,11 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct Landmarks: Hashable, Codable {
+struct Landmark: Hashable, Codable {
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
+    
+    private var imagename: String
+    var Image: Image {
+        SwiftUI.Image(imagename)
+    }
+    //Landmarks構造のユーザーは画像自体だけを気にしているため、プロパティを非公開にします。
 }
