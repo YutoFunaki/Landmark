8d1230ef05dd80684b24ce2ea3324e9464e54b5b
YutoFunaki <104195830+YutoFunaki@users.noreply.github.com>
2022-09-27 23:46:42 +0900

    create Model.swift

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
     var id: Int
@@ -15,9 +16,22 @@ struct Landmark: Hashable, Codable {
     var state: String
     var description: String
     
+    //Landmarks構造のユーザーは画像自体だけを気にしているため、プロパティを非公開にします。
     private var imagename: String
     var Image: Image {
         SwiftUI.Image(imagename)
     }
-    //Landmarks構造のユーザーは画像自体だけを気にしているため、プロパティを非公開にします。
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
 }
---
+++ Landmarks/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Landmarks
+//
+//  Created by 船木勇斗 on 2022/09/27.
+//
+
+import Foundation
